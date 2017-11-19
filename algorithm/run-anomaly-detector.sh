a0f362842de02ea9cf488c59f282fdecf04abad5
zsmester <zsolt.mester67@gmail.com>
2017-11-19 16:43:43 +0100

    Add algorithm/run-anomaly-detector.sh.

---
+++ algorithm/run-anomaly-detector.sh
@@ -0,0 +1,10 @@
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 1 --square_to 999 --action save > log1000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 1000 --square_to 1999 --action save > log2000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 2000 --square_to 2999 --action save > log3000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 3000 --square_to 3999 --action save > log4000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 4000 --square_to 4999 --action save > log5000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 5000 --square_to 5999 --action save > log6000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 6000 --square_to 6999 --action save > log7000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 7000 --square_to 7999 --action save > log8000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 8000 --square_to 8999 --action save > log9000.txt 2>&1
+python3 detect_anomaly.py --training /mnt/disk2/tim-bd-challenge/milano-november/ --testing /mnt/disk2/tim-bd-challenge/milano-december/ --square_from 9000 --square_to 10000 --action save > log10000.txt 2>&1
